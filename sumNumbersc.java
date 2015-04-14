367ff004c969a6cbe7b26d1b5c137fd689a48da0
aswathy <aswathysali1994@gmail.com>
2015-04-14 11:22:29 +0530

    sumNumbersc.java

---
+++ sumNumbersc.java
@@ -0,0 +1,18 @@
+﻿/**
+ * Created by aswathy on 13/4/15.
+ */
+import java.io.*;
+public class sumNumbers {
+    public static void main(String args[])throws Exception
+    {
+        int firstnum;
+        int secnum;
+        int result***;
+        BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("Enter two numbers:");
+        firstnum=Integer.parseInt(br.readLine());
+        secnum=Integer.parseInt(br.readLine());
+        result=firstnum+secnum;
+        System.out.println("The Sum is:"+result);
+    }
+}
